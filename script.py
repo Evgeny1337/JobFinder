5e9a6ba2a88df361c33779a7bab1d23ad6d00b56
evgeny1337 <genya-sozykin@mail.ru>
2024-12-04 22:49:51 +0500

    changed the approach

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import requests
-
+import json
 
 # def get_vacancies_count(area, professional_role, period, languages):
 #     score_result = {}
@@ -36,8 +36,18 @@ def get_vacancies(area, professional_role, language, page=0):
     return json_response
 
 
-def predict_rub_salary(area, professional_role, language, page):
+def predict_rub_salary(area, professional_role, language, page=0):
     json_response = get_vacancies(area, professional_role, language, page)
+    score_result = []
+    for json_item in json_response['items']:
+        salary = json_item['salary']
+        if salary:
+            salary = salary_counter(salary['from'], salary['to'])
+        score_result.append(salary)
+    return score_result
+
+
+def kek_predict_rub_salary(json_response):
     score_result = []
     for json_item in json_response['items']:
         salary = json_item['salary']
@@ -49,23 +59,26 @@ def predict_rub_salary(area, professional_role, language, page):
 
 def get_average_salary(area, professional_role, languages):
     avarage_result = {}
+    all_vacancies = {}
     for language in languages:
-        pages_number = 5
+        pages_number = 10
         page = 0
-        vacancies_count = 0
         nonempty_salary = []
-        nonempty_count = 0
+        # nonempty_count = 0
+        # vacancies_count = get_vacancies(
+        #         area, professional_role, language)['found']
+        all_vacancies[language] = []
         while page < pages_number:
-            vacancies_count += get_vacancies(
-                area, professional_role, language, page)['found']
-            all_salary = predict_rub_salary(area, professional_role, language)
-            nonempty_salary += [salary for salary in all_salary if salary != None]
+            all_vacancies[language].append(get_vacancies(
+                area, professional_role, language, page))
+            # nonempty_salary += [salary for salary in all_salary if salary != None]
             page += 1
-        nonempty_count += len(nonempty_salary)
-        avarage_salary = int(sum(nonempty_salary)/len(nonempty_salary))
-        avarage_result[language] = {"vacancies_found": vacancies_count,
-                                    "vacancies_processed": nonempty_count, "average_salary": avarage_salary}
-    return avarage_result
+    return all_vacancies
+    #     nonempty_count += len(nonempty_salary)
+    #     avarage_salary = int(sum(nonempty_salary)/len(nonempty_salary))
+    #     avarage_result[language] = {"vacancies_found": vacancies_count,
+    #                                 "vacancies_processed": nonempty_count, "average_salary": avarage_salary}
+    # return avarage_result
     # empty_salary_count = all_salary.count(None)
 
 
@@ -73,7 +86,10 @@ def main():
     languages = ['JavaScript', 'Java', 'Python', 'Ruby',
                  'PHP', 'C++', 'C#', 'C', 'Go', 'Objective-C']
     # print(get_vacancies_count(113,96,30))
-    print(get_average_salary(113, 96, languages))
+    kek = get_average_salary(113, 96, languages)
+    vac = kek[languages[1]]
+    for i in vac:
+        print(kek_predict_rub_salary(i))
     # print(predict_rub_salary(113, 96, languages[3]))
 
 
