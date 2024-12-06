966c689a829bd03d774737c14f05c7389b999bc1
Evgeny1337 <genya-sozykin@mail.ru>
2024-12-06 16:13:43 +0300

    Add JobFinder

---
+++ script_jf.py
@@ -0,0 +1,60 @@
+import requests
+from dotenv import load_dotenv
+from os import environ
+
+
+def predict_rub_salary_for_superJob(vacancie):
+    if vacancie['payment_from'] and vacancie['payment_to']:
+        return (vacancie['payment_from'] + vacancie['payment_to'])/2
+    elif vacancie['payment_from'] and not vacancie['payment_to']:
+        return vacancie['payment_from'] * 1.2
+    elif not vacancie['payment_from'] and vacancie['payment_to']:
+        return vacancie['payment_to'] * 0.8
+    return None
+
+def get_vacancies(language,token):
+    headers = {'X-Api-App-Id':token}
+    params={'town':4,'catalogues':48,'keyword':'{}'.format(language),'count':100}
+    response = requests.get('https://api.superjob.ru/2.0/vacancies/',headers=headers,params=params)
+    return response
+
+def get_all_vacancies(languages):
+    token = environ['JOBFINDER_TOKEN']
+    all_vacancies = {}
+    for language in languages:
+        response = get_vacancies(language,token)
+        all_vacancies[language] = response.json()
+    return all_vacancies
+
+def get_avarage_salary(vacancies):
+    avarage_salary = []
+    for vacancie in vacancies:
+        avarage_salary.append(predict_rub_salary_for_superJob(vacancie))
+    nonempty_salary = [salary for salary in avarage_salary if salary != None]
+    return nonempty_salary
+
+
+
+def get_statistic_salary():
+    languages = ['JavaScript', 'Java', 'Python', 'Ruby',
+                 'PHP', 'C++', 'C#', 'C', 'Go', 'Objective-C']
+    avarage_stattistic = {}
+    all_vacancies = get_all_vacancies(languages)
+    for language in languages:
+        vacancies = all_vacancies[language]['objects']
+        salarys = get_avarage_salary(vacancies)
+        avarage_salary = int(sum(salarys)/len(salarys)) if sum(salarys) > 0 else 0
+        count = len(salarys)
+        amount = all_vacancies[language]['total']
+        print(language,salarys,amount)
+        avarage_stattistic[language] = {"vacancies_found":amount,"vacancies_processed":count,"average_salary":avarage_salary}
+    return avarage_stattistic
+
+
+
+def main():
+    load_dotenv()
+    print(get_statistic_salary())
+
+if __name__ == '__main__':
+    main()
